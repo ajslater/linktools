57713830392d832aff9095930a6fe6f8780bb398
AJ Slater <aj@slater.net>
2012-08-23 16:05:14 -0700

    pep8 stuff

--- absolink.py
+++ absolink.py
@@ -6,16 +6,18 @@
 
 __revision__ = '0.1.1'
 
-import os, optparse
+import os
+import optparse
 
-def get_options_and_arguments() :
+
+def get_options_and_arguments():
     """returns the options and the arguments"""
-    usage = "usage: %prog [options]" 
+    usage = "usage: %prog [options]"
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option("-q", "--quiet", action="store_false", \
-        dest="verbose", default = 1, help = "Don't give progress info") 
-    parser.add_option("-r", "--recursive", action="store_true", \
-        dest = "recursive", default = 0, help = "Converts symbolic" \
+    parser.add_option("-q", "--quiet", action="store_false",
+        dest="verbose", default=1, help="Don't give progress info")
+    parser.add_option("-r", "--recursive", action="store_true",
+        dest="recursive", default=0, help="Converts symbolic"
     "links to absolute symbolic links ")
 
     options_and_arguments = parser.parse_args()
@@ -23,30 +25,32 @@ def get_options_and_arguments() :
     return options_and_arguments[0]
 
 
-def abs_link_file(link_file_name, options) :
+def abs_link_file(link_file_name, options):
     """converts the link"""
     link_target_file_name = os.readlink(link_file_name)
-    if (not os.path.isabs(link_target_file_name)) :
+    if (not os.path.isabs(link_target_file_name)):
         absolute_source_file_name = os.path.realpath(link_target_file_name)
 
-        if options.verbose :
+        if options.verbose:
             print("Absolutizing symlink to", absolute_source_file_name)
         os.remove(link_file_name)
         os.symlink(absolute_source_file_name, link_file_name)
 
-def cd_dir(dir_name, options) :
+
+def cd_dir(dir_name, options):
     """finds and descends into subdirectories"""
     os.chdir(dir_name)
-    dir_list = os.listdir(".") #os.getcwd())
- 
-    for file_name in dir_list :
-        if os.path.islink(file_name) :
+    dir_list = os.listdir(".")  # os.getcwd())
+
+    for file_name in dir_list:
+        if os.path.islink(file_name):
             abs_link_file(file_name, options)
-        elif options.recursive and os.path.isdir(file_name): 
+        elif options.recursive and os.path.isdir(file_name):
             cd_dir(file_name, options)
             os.chdir("..")
 
-def main() :
+
+def main():
     """do the deed"""
     options = get_options_and_arguments()
     cd_dir(os.getcwd(), options)
--- hierlink.py
+++ hierlink.py
@@ -1,99 +1,101 @@
 #!/usr/bin/env python
 """
-Hierlink makes a mirrored tree of directories with symlinks to the 
+Hierlink makes a mirrored tree of directories with symlinks to the
 source files
 """
 __revision__ = '0.1.0'
 
-import os, optparse
+import os
+import optparse
 
-def get_options_and_arguments() :
+
+def get_options_and_arguments():
     """parses command line arguments"""
     usage = "usage: %prog [options] <source directory>"
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option("-q", "--quiet", action="store_false", dest="verbose", 
+    parser.add_option("-q", "--quiet", action="store_false", dest="verbose",
         default=1, help="Do not display output")
     parser.add_option("-s", "--symlink", action="store_true", dest="symlink",
         default=0, help="crease symbolic links intead of hard links")
     parser.add_option("-f", "--flatten", action="store_true", dest="flatten",
-        default=0, help="Does not create a directory structure on the"+
-    "destination side, just a big flat collection of links to the"+ 
+        default=0, help="Does not create a directory structure on the" +
+    "destination side, just a big flat collection of links to the" +
     "source tree")
 
     (options, arguments) = parser.parse_args()
-  
-    if len(arguments) <= 0 :
+
+    if len(arguments) <= 0:
         parser.print_help()
         exit(1)
 
     return (options, arguments)
 
 
-def ln_file(source_filename, dest_filename, options) :
+def ln_file(source_filename, dest_filename, options):
     """Links the target file or creates a mirror directory"""
     dest_dirname = os.path.dirname(dest_filename)
 
-    if options.verbose :
-        if options.symlink :
+    if options.verbose:
+        if options.symlink:
             pref = 'Syml'
-        else :
+        else:
             pref = 'L'
 
-    print(pref+'inking '+source_filename+' to '+dest_filename)
+    print(pref + 'inking ' + source_filename + ' to ' + dest_filename)
 
-    if not os.path.exists(dest_filename) :
-        if not os.path.exists(dest_dirname) :
-            if options.verbose :                               
+    if not os.path.exists(dest_filename):
+        if not os.path.exists(dest_dirname):
+            if options.verbose:
                 print('Creating', dest_dirname, 'from', source_filename)
             os.mkdir(dest_dirname)
-      
-        if options.symlink :
+
+        if options.symlink:
             os.symlink(source_filename, dest_filename)
-        else :
+        else:
             os.link(source_filename, dest_filename)
 
 
-def cd_dir(source_dirname, dest_dirname, options, relpath) :
+def cd_dir(source_dirname, dest_dirname, options, relpath):
     """Traverses the source directory tree"""
-    if (not os.path.isdir(dest_dirname)) :
+    if (not os.path.isdir(dest_dirname)):
         os.mkdir(dest_dirname)
     os.chdir(dest_dirname)
     source_dirlist = os.listdir(source_dirname)
     source_dirlist.sort()
-  
-    for filename in source_dirlist :
-        source_filename = source_dirname+os.sep+filename
-        dest_filename = dest_dirname+os.sep+filename
-    
-        if os.path.isdir(source_filename) :
-            if relpath and not options.flatten :
-                newsource_dirname = os.pardir+os.sep+source_filename
-            else :
+
+    for filename in source_dirlist:
+        source_filename = source_dirname + os.sep + filename
+        dest_filename = dest_dirname + os.sep + filename
+
+        if os.path.isdir(source_filename):
+            if relpath and not options.flatten:
+                newsource_dirname = os.pardir + os.sep + source_filename
+            else:
                 newsource_dirname = source_filename
 
-            if options.flatten :
+            if options.flatten:
                 newdest_dirname = dest_dirname
-            else :
+            else:
                 newdest_dirname = dest_filename
-      
+
             cd_dir(newsource_dirname, newdest_dirname, options, relpath)
-        else :
+        else:
             ln_file(source_filename, dest_filename, options)
 
 
-def main() :
+def main():
     """main"""
-    (options, arguments)=get_options_and_arguments()
-  
-    if len(arguments) > 0 :
+    (options, arguments) = get_options_and_arguments()
+
+    if len(arguments) > 0:
         source_dir = os.path.normpath(arguments[0])
         relpath = os.path.abspath(source_dir) != source_dir
-    else :
+    else:
         relpath = 0
 
-    if options.verbose and relpath :
+    if options.verbose and relpath:
         print('Source directory', source_dir, 'is a relative path.')
- 
+
         cd_dir(source_dir, os.getcwd(), options, relpath)
 
 if __name__ == '__main__':
