5cb5a7d995c07336d6a0ae910da6398540deb4c7
AJ Slater <aj@slater.net>
2016-07-31 13:10:00 -0700

    massive speedup using find

--- flatlink.sh
+++ flatlink.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-IFS=$'\n'
 SRC_DIR="$1"
 TARGET_DIR="$2"
 
@@ -9,32 +8,9 @@ usage() {
   echo "Recursively walks <SOURCE_DIR> and creates symbolic links of all those files into one flat directory, <TARGET_DIR>"
 }
 
-function link_if_new() {
-  src_path=$(realpath "$1")
-  src_fn="${src_path##*/}"
-  dst_path="$TARGET_DIR"/"$src_fn"
-  if [[ ! -e "$dst_path" || "$src_path" != $(realpath "$dst_path") ]]; then
-    echo ln -sf "$src_path" "$TARGET_DIR"/
-    ln -sf "$src_path" "$TARGET_DIR"
-  else
-    echo -n .
-  fi
-}
-
-function linkem() {
-  src_dir=$1
-  for fn in $src_dir/*; do
-    if [[ -d "$fn" ]]; then
-      linkem "$fn"
-    else
-      link_if_new "$fn"
-    fi
-  done
-}
-
-if [[ -z "$1" || -z "$2" ]]; then
+if [[ -z "$SRC_DIR" || -z "$TARGET_DIR" ]]; then
     usage
     exit 1
 fi
 
-linkem "$SRC_DIR"
+find "$SRC_DIR" -type f -print0 | xargs -0 -I {} ln -sf {} "$TARGET_DIR"
